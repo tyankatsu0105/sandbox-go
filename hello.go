32ccdcc38fc2ff424215203e03c9ad6496f16ece
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-06-25 18:13:12 +0900

    007

--- hello.go
+++ hello.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	fmt.Println("Hello")
+	var isTrue bool = true
+
+	fmt.Println(isTrue)
+
+	var i int = 12345
+	var int64 int64 = int64(i)
+
+	fmt.Println(int64)
+
+	var str string = "あいうえ"
+	str += "お"
+
+	fmt.Println(str)
+
+	var en string = "golang"
+	var ja string = "Go言語"
+
+	fmt.Println(en, " length: ", len(en))
+	fmt.Println(ja, " length: ", utf8.RuneCountInString(ja))
 }
