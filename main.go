3477b85deabd208032992a08f55165131c7feb7b
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-11-07 16:58:46 +0900

    receiever

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/tyankatsu0105/sandbox-go/src/math"
+	"math"
 )
 
+type Rectangle struct {
+	width, height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+func (c Circle) area() float64 {
+	return c.radius * c.radius * math.Pi
+}
+
 func main() {
-	fmt.Println("AAAAAAAA")
-	a := math.Sum(10, 20)
-	fmt.Println(a)
+	r1 := Rectangle{width: 12, height: 2}
+	r2 := Rectangle{width: 9, height: 4}
+	c1 := Circle{radius: 10}
+	c2 := Circle{radius: 25}
+
+	fmt.Println("r1", r1.area())
+	fmt.Println("r2", r2.area())
+	fmt.Println("c1", c1.area())
+	fmt.Println("c2", c2.area())
 }
