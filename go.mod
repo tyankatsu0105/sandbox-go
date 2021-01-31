f0e6e638661746108e60ccaa921518bb8d3d5f07
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-01-31 14:48:28 +0900

    init

--- go.mod
+++ go.mod
@@ -2,7 +2,3 @@ module github.com/tyankatsu0105/sandbox-go
 
 go 1.15
 
-require (
-	github.com/julienschmidt/httprouter v1.3.0
-	golang.org/x/tools v0.1.0 // indirect
-)
--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello tyankatsu!")
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "login")
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", sayhelloName)
-	http.HandleFunc("/login", login)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	fmt.Println("Hello")
 }
